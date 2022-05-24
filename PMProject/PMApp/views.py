5550720eeb0d15cc31b3160a3b8144ed79e52791
Faisal othman <Faisalalsneed@gmail.com>
2022-05-24 23:41:34 +0300

    add

---
+++ PMProject/PMApp/views.py
@@ -0,0 +1,14 @@
+from distutils.command.install import value
+
+from django.shortcuts import render
+
+# Create your views here.
+from PMProject.PMApp.models import Task
+
+'''
+def List():
+    Task.objects.all()
+    for i in Task:
+      lst=[i]
+      c={i}
+    return render('base.html',{c})'''
